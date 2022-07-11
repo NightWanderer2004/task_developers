4be0e92f671f4ce6929cc27bbd059dc7451a121d
Eduard <goby145csgo@gmail.com>
2022-07-12 01:16:26 +0200

    add slider components

---
+++ src/components/UI/ImageBlock.jsx
@@ -0,0 +1,15 @@
+import s from '../../styles/UI/imageBlock.module.scss'
+
+const ImageBlock = el => {
+   return (
+      <div className={s.infoImage}>
+         <div className={s.backdrop}>
+            <h3 className={s.titleImage}>Онлайн-магазин</h3>
+            <p>{el.el.title}</p>
+         </div>
+         <img src={`/assets/img/portfolio/${el.el.image}.png`} />
+      </div>
+   )
+}
+
+export default ImageBlock
