23ea3ea2773940e8703c6d5cffb694958087791d
Eduard <goby145csgo@gmail.com>
2022-07-20 19:20:40 +0200

    fix img 'alt'

--- src/components/UI/ImageBlock.jsx
+++ src/components/UI/ImageBlock.jsx
@@ -7,7 +7,11 @@ const ImageBlock = el => {
             <h3 className={s.titleImage}>Онлайн-магазин</h3>
             <p>{el.el.title}</p>
          </div>
-         <img loading="lazy" src={`/assets/img/portfolio/${el.el.image}.png`} />
+         <img
+            loading="lazy"
+            src={`/assets/img/portfolio/${el.el.image}.png`}
+            alt={el.el.image}
+         />
       </div>
    )
 }
